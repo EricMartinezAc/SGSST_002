c2584a96ee39eb3d48d44de6a006ed1c3ba1e89e
Eric Martinez Acelas <ise.projects@outlook.com>
2020-12-28 21:52:47 -0500

    reparada funcionalidad progressbar inicio sesion

--- app/public/js/login.js
+++ app/public/js/login.js
@@ -1,30 +1,31 @@
 const login = (datoUsu, psw) => {
 
+    //levantar progressbar:
+    document.getElementById('contentbarProgress').style.display = 'inline';
+    document.getElementById('progress-bar').style.width = '75%';
+
+    //validar campos:
     let ResultVal = ValidacionesDeCampo(datoUsu, psw)
 
+    //si campos son cumplen especificaciones:
     if (ResultVal.length >= 1) {
 
+        //crear websocket:
         let ws = new WebSocket('ws://localhost:8002')
 
+        //en conexión de websocket, enviar datos:
         ws.onopen = function (e) {
 
-            //waiting_bar
-            document.getElementById('contentbarProgress').style.display = 'inline';
+            console.log("Conexión establecida")
 
-            setInterval(() => {
-                document.getElementById('progress-bar').style.width = '75%';
-                setInterval(() => {
-                    let datos = {
-                        _process_: 'read',
-                        _data_: [datoUsu, psw]
-                    }
-                    console.log("Conexión establecida")
-                    ws.send(JSON.stringify(datos));
-                    console.log(`Enviado ${JSON.stringify(datos)} al servidor`);
-                }, 2000);
-            }, 2000);
+            //enviar datos
+            let datos = { _process_: 'read', _data_: [datoUsu, psw] }
+            ws.send(JSON.stringify(datos));
+            console.log(`Enviado ${JSON.stringify(datos)} al servidor`);
+
         }
 
+        //en escuha de mensajes, recibir datos y ejecutar acciones:
         ws.onmessage = function (event) {
             //complete_bar
             document.getElementById('progress-bar').style.width = '100%';
@@ -40,29 +41,43 @@ const login = (datoUsu, psw) => {
             }, 2000);
         };
 
+        //al cerrar websocket:
         ws.onclose = function (event) {
-            document.getElementById('progress-bar').style.width = '100%';
+            //tumbar progressbar
+            document.getElementById('progress-bar').style.width = '0%';
             setInterval(() => {
                 if (event.wasClean) {
-                    alert(`Conexión cerrada y limpiada, code=${event.code} reason=${event.reason}`);
+                    //alert(`Conexión cerrada y limpiada, code=${event.code} reason=${event.reason}`);
+                    conexion_ws_closeclean(event)
+                    //cerrar progressvar
                     document.getElementById('contentbarProgress').style.display = 'none';
-                    document.getElementById('progress-bar').style.width = '0%';
                 } else {
                     // e.g. server process killed or network down
-                    // event.code is usually 1006 in this case
-                    alert('Conexión cortada');
+                    // event.code is usually 1006 in this case1
+                    conexion_ws_cortada()
+                    //cerrar progressvar
                     document.getElementById('contentbarProgress').style.display = 'none';
-                    document.getElementById('progress-bar').style.width = '0%';
-                }
+                };
+                //redireccionar a inicio
+                setInterval(() => {
+                    window.location = '/'
+                }, 800);
             }, 2000);
-        };
+        }
 
+        //si existe un error en websocket:
         ws.onerror = function (error) {
-            document.getElementById('progress-bar').style.width = '100%';
+            //tumbar progressbar
+            document.getElementById('progress-bar').style.width = '0%';
             setInterval(() => {
+                //cerrar progressvarsubWind_alert_conexWS_cant
+                //alert(`No se pudo conectar, conexión: ${error.message}, `);
+                cantConect(error)
                 document.getElementById('contentbarProgress').style.display = 'none';
-                document.getElementById('progress-bar').style.width = '0%';
-                alert(`No se pudo conectar, conexión: ${error.message}, `);
+                //redireccionar a inicio
+                setInterval(() => {
+                    window.location = '/'
+                }, 800);
             }, 2000);
         };
 
@@ -75,4 +90,14 @@ function ValidacionesDeCampo(datos, passw) {
     let mnjs = null;
     mnjs = 'aprobado'
     return mnjs
+}
+
+function conexion_ws_closeclean(evento) {
+}
+
+function conexion_ws_cortada() {
+}
+
+function cantConect(errorC) {
+    
 }
