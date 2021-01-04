a07d8e28b94043419f18a798f91cd627a0d63858
dev_hp_eric <dev_hp_eric@iseeg.com>
2021-01-03 22:06:38 -0500

    permite consulta, aplicada logica de autenticacion con db y backend

--- app/public/js/login.js
+++ app/public/js/login.js
@@ -24,76 +24,262 @@ const login = (datoUsu, psw) => {
             console.log(`Enviado ${JSON.stringify(datos)} al servidor`);
 
         }
-
         //en escuha de mensajes, recibir datos y ejecutar acciones:
         ws.onmessage = function (event) {
-            let resultado = JSON.parse(JSON.stringify(JSON.parse(event.data).resp_))[2];
 
-            if (JSON.parse(JSON.stringify(JSON.parse(event.data).error_)) === false) {//sin error de proceso
+            console.log('recibiendo y examinando resultado ... ')
 
-                if (JSON.parse(JSON.stringify(JSON.parse(event.data).resp_))[1]) {//Existe resultado de consulta valida
+            //recolectando datos de resultado
+            let resultado = JSON.parse(JSON.stringify(JSON.parse(event.data)));
+            let estadoErrorConexion = resultado.error_
+            let usuariostack = resultado.resp_[0]
+            let mjs_result = resultado.resp_[1]
+            let estado_consulta = resultado.resp_[2]
+            console.log(mjs_result)
 
-                    if (resultado === "success") {// estado de consulta
-                        //complete_bar
-                        document.getElementById('progress-bar').style.width = '100%';
-                        //mostrar datos retornados
-                        console.log(`Datos retornados desde el servidor: ${JSON.parse(JSON.stringify(JSON.parse(event.data).resp_))[1]}`);
-                        //ocultar progressbar
-                        setInterval(() => {
-                            document.getElementById('contentbarProgress').style.display = 'none';
-                            connected()
-                        }, 2000);
-                    } 
-
-                } 
-                
-            } else {
-                cantConect(' :xx: error de proceso')
-            }
-        };
-
-        //al cerrar websocket:
-        /*
-        ws.onclose = function (event) {
-            //tumbar progressbar
-            document.getElementById('progress-bar').style.width = '0%';
             setInterval(() => {
-                if (event.wasClean) {
-                    //cerrar progressvar
-                    document.getElementById('contentbarProgress').style.display = 'none';
-                    //mostar mensaje alerta
+                //stage 1: si se conectó sin error
+                if (estadoErrorConexion === false) {
+                    //stage 2: si error de consulta -> resolve(Error de consulta)
+                    if (mjs_result !== 'Error de consulta') {
+                        //stage 3: si datos erroneos -> resolve(datos no encontrados)
+                        if (estado_consulta === false) {
+                            //tumbar todo
+                            setInterval(() => {
+                                alertMjsError(mjs_result)
+                            }, 500);
+                        }
+                        //stage 4:  datos encontrados -> resolve(Bienvenido al administardor de SG-SST)
+                        if (estado_consulta === true) {
+                            //complete_bar
+                            document.getElementById('progress-bar').style.width = '100%';
+                            //mostrar datos retornados
+                            console.log(`Datos retornados desde el servidor: ${usuariostack}`);
+                            //ocultar progressbar
+                            setInterval(() => {
+                                authSuccess(mjs_result)
+                            }, 600);
+                        }
+                    }
+                    //stage 2: si hubo error de consulta
+                    if (mjs_result === 'Error de consulta') {
+                        //tumbar todo
+                        document.getElementById('progress-bar').style.width = '4%';
+                        document.getElementById('progress-bar').style.width = '0%';
+                        setInterval(() => {
+                            alertMjsError(mjs_result)
+                        }, 500);
+                    }
+                }
+                //stage 1: si hubo error de conexion
+                if (estadoErrorConexion === true) {
+                    //tumbar todo
+                    document.getElementById('progress-bar').style.width = '0%';
                     setInterval(() => {
-                        //alert(`Conexión cerrada y limpiada, code=${event.code} reason=${event.reason}`);
-                        conexion_ws_closeclean(event)
+                        alertMjsError(mjs_result)
                     }, 500);
-                } else {
-                    // e.g. server process killed or network down
-                    // event.code is usually 1006 in this case1
-                    conexion_ws_cortada()
-                    //cerrar progressvar
-                    document.getElementById('contentbarProgress').style.display = 'none';
-                };
-            }, 4000);
-        }
-        */
+                }
 
+            }, 2000);
+
+        }
         //si existe un error en websocket:
         ws.onerror = function (error) {
             //tumbar progressbar
             document.getElementById('progress-bar').style.width = '0%';
-            cantConect(error)
+            setInterval(() => {
+                cantConectWS(error)
+            }, 1000);
         };
 
-    } else {
+    } else {//Valores de entrada no cumplen condiciones
         alert(ResultVal)
     }
 }
 
+//FUNCIONES PARA FUNCIONALIDADES ADICIONAES
+
+//validar campos
 function ValidacionesDeCampo(datos, passw) {
     let mnjs = null;
     mnjs = 'aprobado'
     return mnjs
 }
+
+//no se pudo conectar a websocket
+function cantConectWS(errorC) {
+    //cerrar progressvarsubWind_alert_conexWS_cant
+    document.getElementById('contentbarProgress').style.display = 'none';
+
+    //alert(`No se pudo conectar, conexión: ${error.message}, `);
+    document.getElementById('subWind_alert_conexWS_cant_errorMessage').innerHTML = errorC.message;
+
+    //cerrar cualquier otra ventana y abrir "no se pud conectar WS"
+    document.getElementById('contentsubWind_alert_conexWS_cant').style.display = 'inline';
+    document.getElementById('contentsubWind_alert_auth_cant').style.display = 'none';
+    document.getElementById('contentsubWind_alert_auth_success').style.display = 'none';
+
+    //redireccionar a index
+    setInterval(() => {
+        window.location = '/'
+    }, 1550);
+
+}
+
+//errores en conexion y consulta base de datos
+function alertMjsError(msj) {
+    //cerrar progressvarsubWind_alert_conexWS_cant
+    document.getElementById('contentbarProgress').style.display = 'none';
+
+    //alert(`No se pudo conectar, conexión: ${error.message}, `);
+    document.getElementById('subWind_alert_errorMessage').innerHTML = msj;
+    //cerrar cualquier otra ventana abrir ventana de alerta mostrando el mensaje configurado
+    document.getElementById('contentsubWind_alert_auth_cant').style.display = 'none';
+    document.getElementById('contentsubWind_alert_auth_cant').style.display = 'inline';
+    document.getElementById('contentsubWind_alert_auth_success').style.display = 'none';
+
+    //redireccionar a index
+    setInterval(() => {
+        window.location = '/'
+    }, 1500);
+
+}
+
+//autenticado con exito
+function authSuccess(msjA) {
+    //cerrar progressvarsubWind_alert_conexWS_cant
+    document.getElementById('contentbarProgress').style.display = 'none';
+
+    document.getElementById('subWind_alert_successMessage').innerHTML = msjA;
+    //cerrar cualquier otra ventana y abrir "success"
+    document.getElementById('contentsubWind_alert_conexWS_cant').style.display = 'none';
+    document.getElementById('contentsubWind_alert_auth_cant').style.display = 'none';
+    document.getElementById('contentsubWind_alert_auth_success').style.display = 'inline';
+
+    //redireccionar a index
+    setInterval(() => {
+        window.location = '/dashboard'
+    }, 1000);
+}
+/*
+//function error no tener conexion
+function cantConect(errorC) {
+    setInterval(() => {
+        //cerrar progressvarsubWind_alert_conexWS_cant
+        document.getElementById('contentbarProgress').style.display = 'none';
+
+        //alert(`No se pudo conectar, conexión: ${error.message}, `);
+        document.getElementById('subWind_alert_conexWS_cant_errorMessage').innerHTML = errorC.message;
+
+
+        document.getElementById('contentsubWind_alert_auth_cant').style.display = 'none';
+
+        document.getElementById('contentsubWind_alert_conexWS_cortada').style.display = 'none';
+        document.getElementById('contentsubWind_alert_conexWS_cant').style.display = 'inline';
+        document.getElementById('contentsubWind_alert_conexWS_closeclean').style.display = 'none';
+        document.getElementById('contentsubWind_alert_conexWS').style.display = 'none';
+
+        //redireccionar a index
+        setInterval(() => {
+            window.location = '/'
+        }, 2000);
+    }, 1000);
+}
+
+//function se logró conexión
+function connected() {
+    setInterval(() => {
+
+        //alert success;
+        document.getElementById('contentsubWind_alert_conexWS_cortada').style.display = 'none';
+
+        document.getElementById('contentsubWind_alert_auth_cant').style.display = 'none';
+
+        document.getElementById('contentsubWind_alert_conexWS_cant').style.display = 'none';
+        document.getElementById('contentsubWind_alert_conexWS_closeclean').style.display = 'none';
+        document.getElementById('contentsubWind_alert_conexWS').style.display = 'inline';
+
+        //redireccionar a index
+        setInterval(() => {
+            window.location = '/dashboard'
+        }, 1500);
+    }, 1000);
+}
+
+//function error en consulta
+function cantQuery(errorC) {
+    setInterval(() => {
+        //cerrar progressvarsubWind_alert_conexWS_cant
+        document.getElementById('contentbarProgress').style.display = 'none';
+
+        //alert(`No se pudo conectar, conexión: ${error.message}, `);
+        document.getElementById('subWind_alert_auth_cant_errorMessage').innerHTML = errorC;
+
+
+        document.getElementById('contentsubWind_alert_auth_cant').style.display = 'inline';
+
+        document.getElementById('contentsubWind_alert_conexWS_cortada').style.display = 'none';
+        document.getElementById('contentsubWind_alert_conexWS_cant').style.display = 'none';
+        document.getElementById('contentsubWind_alert_conexWS_closeclean').style.display = 'none';
+        document.getElementById('contentsubWind_alert_conexWS').style.display = 'none';
+
+        //redireccionar a index
+        setInterval(() => {
+            window.location = '/'
+        }, 2000);
+    }, 1000);
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//al cerrar websocket:
+/*
+ws.onclose = function (event) {
+    //tumbar progressbar
+    document.getElementById('progress-bar').style.width = '0%';
+    setInterval(() => {
+        if (event.wasClean) {
+            //cerrar progressvar
+            document.getElementById('contentbarProgress').style.display = 'none';
+            //mostar mensaje alerta
+            setInterval(() => {
+                //alert(`Conexión cerrada y limpiada, code=${event.code} reason=${event.reason}`);
+                conexion_ws_closeclean(event)
+            }, 500);
+        } else {
+            // e.g. server process killed or network down
+            // event.code is usually 1006 in this case1
+            conexion_ws_cortada()
+            //cerrar progressvar
+            document.getElementById('contentbarProgress').style.display = 'none';
+        };
+    }, 4000);
+}
+*/
+
+
 
 //function al cerrar websocket:
 /*
@@ -113,40 +299,3 @@ function conexion_ws_cortada() {
     document.getElementById('contentsubWind_alert_conexWS_closeclean').style.display = 'none';
 }
 */
-
-//function al no tener conexion
-function cantConect(errorC) {
-    setInterval(() => {
-        //cerrar progressvarsubWind_alert_conexWS_cant
-        document.getElementById('contentbarProgress').style.display = 'none';
-
-        //alert(`No se pudo conectar, conexión: ${error.message}, `);
-        document.getElementById('subWind_alert_conexWS_cant_errorMessage').innerHTML = errorC.message;
-        document.getElementById('contentsubWind_alert_conexWS_cortada').style.display = 'none';
-        document.getElementById('contentsubWind_alert_conexWS_cant').style.display = 'inline';
-        document.getElementById('contentsubWind_alert_conexWS_closeclean').style.display = 'none';
-        document.getElementById('contentsubWind_alert_conexWS').style.display = 'none';
-
-        //redireccionar a index
-        setInterval(() => {
-            window.location = '/'
-        }, 2000);
-    }, 1000);
-}
-
-//function se logró conexión
-function connected() {
-    setInterval(() => {
-
-        //alert success;
-        document.getElementById('contentsubWind_alert_conexWS_cortada').style.display = 'none';
-        document.getElementById('contentsubWind_alert_conexWS_cant').style.display = 'none';
-        document.getElementById('contentsubWind_alert_conexWS_closeclean').style.display = 'none';
-        document.getElementById('contentsubWind_alert_conexWS').style.display = 'inline';
-
-        //redireccionar a index
-        setInterval(() => {
-            window.location = '/dashboard'
-        }, 1500);
-    }, 1000);
-}
